f576316dc14fba26307dff2e5495cdfbd68c3579
Hugo SABOURIN <hsabouri@e1r4p2.42.fr>
2020-01-09 16:19:05 +0100

    parser v1 done

--- src/ast.rs
+++ src/ast.rs
@@ -1,27 +1,54 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-/// Converts a Vec<(T, U)> in a HashMap<T, U>. Similar to Haskell function `fromList` in Data.Strict.Map module.
-pub fn from_vec<T: Eq + Hash, U>(list: Vec<(T, U)>) -> HashMap<T, U> {
+/// Converts a Vec<(T, U)> in a HashMap<T, U>
+pub fn convert<T: Eq + Hash, U>(input: Vec<(T, U)>) -> HashMap<T, U> {
     let mut res: HashMap<T, U> = HashMap::new();
 
-    for (t, u) in list.into_iter() {
+    for (t, u) in input.into_iter() {
         res.insert(t, u);
     }
     res
 }
 
+pub type Inventory = HashMap<String, u32>;
+
+#[derive(Debug)]
 pub struct Process {
     name: String,
-    input: HashMap<String, u32>,
-    output: HashMap<String, u32>,
+    input: Inventory,
+    output: Inventory,
     duration: u32,
 }
 
+impl Process {
+    pub fn new(name: String, input: Inventory, output: Inventory, duration: u32) -> Self {
+        Self {
+            name,
+            input,
+            output,
+            duration
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Simulation {
-    inventory: HashMap<String, u32>,
-    processes: HashMap<String, u32>,
+    inventory: Inventory,
+    processes: Vec<Process>,
     optimize: Vec<String>,
     optimize_time: bool,
 }
 
+impl Simulation {
+    pub fn new(inventory: Inventory, processes: Vec<Process>, optimize: (Vec<String>, bool)) -> Self {
+        let (optimize, optimize_time) = optimize;
+
+        Self {
+            inventory,
+            processes,
+            optimize,
+            optimize_time,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
-//mod parser;
+use std::fs::File;
+use std::io::Read;
+
+mod parser;
 mod ast;
 
+use ast::Simulation;
+
+use parser::SimulationParser;
+
+fn parse(content: String) {
+    let parser = SimulationParser::new().parse(&content);
+
+    match parser {
+        Ok(sim) => { println!("{:#?}", sim); },
+        Err(err) => { println!("{}", err); }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = "factorio.sim";
+
+    match File::open(file) {
+        Ok(mut file) => {
+            let mut content = String::new();
+
+            file.read_to_string(&mut content).unwrap();
+
+            println!("{}", content);
+
+            parse(content);
+        },
+        Err(error) => {
+            println!("{}", error);
+        },
+    }
 }
