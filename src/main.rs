64d41292e85c441ebcd61ddc94e961cf259a80b1
Hugo SABOURIN <hsabouri@student.42.fr>
2020-01-11 12:39:24 +0100

    main return Result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(process_exitcode_placeholder)]
+
 use std::fs::File;
 use std::io::Read;
 
@@ -6,20 +8,16 @@ use clap::{Arg, App};
 mod parser;
 mod ast;
 
+use parser::SimulationParser;
 use ast::Simulation;
 
-use parser::SimulationParser;
-
-fn parse(content: String) {
-    let parser = SimulationParser::new().parse(&content);
-
-    match parser {
-        Ok(sim) => { println!("{:#?}", sim); },
-        Err(err) => { println!("{}", err); }
-    }
+fn parse(content: String) -> Result<Simulation, String> {
+    SimulationParser::new()
+        .parse(&content)
+        .map_err(|err| format!("{}", err))
 }
 
-fn main() {
+fn main() -> Result<(), ()> {
     let matches = App::new("krpsim")
         .author("Hugo Sabourin <hsabouri@student.42.fr>")
         .about("Process optimizer")
@@ -28,26 +26,32 @@ fn main() {
             .required(true)
             .index(1))
         .arg(Arg::with_name("DELAY")
-            .help("Time of the simulation")
+            .help("Duration of the simulation")
             .required(false)
             .index(2))
         .get_matches();
+    let file = matches
+        .value_of("FILE")
+        .unwrap();
+    let delay = matches
+        .value_of("DELAY")
+        .unwrap_or("0")
+        .parse()
+        .unwrap_or(0);
 
-    let file = matches.value_of("FILE").unwrap();
-    let delay = matches.value_of("DELAY").unwrap_or("0").parse().unwrap_or(0);
-
-    match File::open(file) {
+    let simulation = match File::open(file) {
         Ok(mut file) => {
             let mut content = String::new();
 
             file.read_to_string(&mut content).unwrap();
-
-            println!("{}", content);
-
-            parse(content);
+            parse(content)
         },
-        Err(error) => {
-            println!("{}", error);
-        },
-    }
+        Err(error) => Err(format!("{}", error)),
+    };
+
+    simulation
+        .map(|sim| {})
+        .map_err(|err| {
+            println!("{}", err);
+        })
 }
