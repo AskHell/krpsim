9f4ae6be8e111fa194725f8aaa3c89293594c8cf
Hugo SABOURIN <hsabouri@student.42.fr>
2020-01-16 15:51:41 +0100

    compute combination of processes for `get_child`

--- src/ast.rs
+++ src/ast.rs
@@ -13,6 +13,43 @@ pub fn convert<T: Eq + Hash, U>(input: Vec<(T, U)>) -> HashMap<T, U> {
 
 pub type Inventory = HashMap<String, u32>;
 
+pub fn inventory_add(left: &Inventory, right: &Inventory) -> Inventory {
+    let mut res: Inventory = HashMap::new();
+
+    for (key, value) in left.into_iter() {
+        res.insert(key.clone(), match right.get(key) {
+            Some(right_value) => value + right_value,
+            None => value.clone()
+        });
+    }
+    res
+}
+
+pub fn inventory_sub_process(inventory: &Inventory, process: &Process) -> Inventory {
+    let mut res: Inventory = HashMap::new();
+
+    for (key, value) in process.input.iter() {
+        if let Some(has) = inventory.get(key) {
+            if has - value > 0 {
+                res.insert(key.clone(), has - value);
+            }
+        }
+    }
+    res
+}
+
+pub fn inventory_compare(left: &Inventory, right: &Inventory) -> bool {
+    left.iter().fold(true, |acc, (key, value)| {
+        if acc == false {false}
+        else {
+            match right.get(key) {
+                Some(we_got) => we_got >= value,
+                None => false
+            }
+        }
+    })
+}
+
 #[derive(Debug, Clone)]
 pub struct Process {
     pub name: String,
@@ -34,10 +71,10 @@ impl Process {
 
 #[derive(Debug)]
 pub struct Simulation {
-    inventory: Inventory,
-    processes: Vec<Process>,
-    optimize: Vec<String>,
-    optimize_time: bool,
+    pub inventory: Inventory,
+    pub processes: Vec<Process>,
+    pub optimize: Vec<String>,
+    pub optimize_time: bool,
 }
 
 impl Simulation {
--- src/solver.rs
+++ src/solver.rs
@@ -1,7 +1,10 @@
 use super::ast::{
     Simulation,
     Process,
-    Inventory
+    Inventory,
+    inventory_add,
+    inventory_compare,
+    inventory_sub_process
 };
 
 fn max_of_iter<T: Ord>(mut i: impl Iterator<Item = T>) -> T {
@@ -14,23 +17,116 @@ fn max_of_iter<T: Ord>(mut i: impl Iterator<Item = T>) -> T {
 struct Node {
     pub parent: usize,
     pub inventory: Inventory,
-    pub input: Vec<(Process, u32)>
-    pub ouput: Vec<(Process, u32)>,
+    pub input: Vec<(Process, u32)>,
+    pub output: Vec<(Process, u32)>,
     pub h: u32,
     pub time: u32,
 }
 
 impl Node {
-    fn add_inventories() -> Inventory {
-
+    fn separate_processes(processes: Vec<(Process, u32)>, time: u32) -> (Vec<Process>, Vec<Process>) {
+        processes.into_iter()
+            .fold((Vec::<Process>::new(), Vec::<Process>::new()), |(mut finished, mut active), (p, t)| {
+                if t <= time {
+                    finished.push(p)
+                } else {
+                    active.push(p)
+                }
+                (finished, active)
+            })
     }
 
-    pub fn from_parent(parent: &Self) -> Self {
-        let time = parent.ouput
+    fn get_available_processes(inventory: &Inventory, simulation: &Simulation, time: u32) -> Vec<(Process, u32)> {
+        simulation.processes.clone().into_iter().filter_map(|p| {
+            match inventory_compare(&p.input, inventory) {
+                true => Some(p),
+                false => None,
+            }
+        })
+        .map(|p| {
+            let end = p.duration + time;
+
+            (p, end)
+        })
+        .filter_map(|(p, t)| {
+            Some((p, t)) // TODO: Is end-time over simulation duration
+        })
+        .collect()
+    }
+
+
+    fn get_possible_output_closure(
+        mut acc: Vec<(Vec<(Process, u32)>, Inventory)>,
+        mut actual: Vec<(Process, u32)>,
+        inventory: Inventory,
+        simulation: &Simulation,
+        time: u32
+    ) -> Vec<(Vec<(Process, u32)>, Inventory)> {
+        let available = Self::get_available_processes(&inventory, simulation, time);
+
+        acc.push((actual.clone(), inventory.clone()));
+
+        if available.len() > 0 {
+            for (process, process_end) in available.into_iter() {
+                let last = actual.pop();
+
+                let explore = match last {
+                    Some((act_p, act_t)) => if process_end <= act_t {
+                        actual.push((act_p, act_t));
+                        true
+                    } else {
+                        actual.push((act_p, act_t));
+                        false
+                    },
+                    None => {
+                        true
+                    }
+                };
+                if explore {
+                    actual.push((process.clone(), process_end));
+                    acc = Self::get_possible_output_closure(
+                        acc,
+                        actual.clone(),
+                        inventory_sub_process(&inventory, &process),
+                        simulation,
+                        time
+                    );
+                }
+            }
+        }
+        acc
+    }
+
+    /// Returns a restrained list of all unique possible combination of processes.
+    /// Unique in such a way that if a combination `a:(2, 1, 1)` exists, a similar combination but ordered differently is not possible (ex: `b:(1, 2, 1)`)
+    /// The process used to avoid similar combination returns a sorted list as a side effect.
+    fn get_possible_output(inventory: &Inventory, simulation: &Simulation, time: u32) -> Vec<(Vec<(Process, u32)>, Inventory)> {
+        Self::get_possible_output_closure(
+            Vec::new(),
+            Vec::new(),
+            inventory.clone(),
+            simulation,
+            time
+        )
+    }
+
+    /* // Hidden for commit and tests
+    pub fn get_childs(&self, simulation: &Simulation) -> Vec<Self> {
+        let time = self.output
             .into_iter()
             .map(|(p, t)| t)
-            .min();
+            .min()
+            .unwrap();
 
-        let mut inventory = parent.inventory;
+        let (finished, input) = Self::separate_processes(self.output, time);
+
+        let new_inventory = finished
+            .into_iter()
+            .fold(self.inventory.clone(), |acc, p| {
+                inventory_add(&acc, &p.output)
+            });
+
+        let possible_output = Self::get_possible_output(new_inventory, simulation, time);
     }
+    */
 }
